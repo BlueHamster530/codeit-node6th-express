621976f7cbaf03db707269f3e0d5f9b4e83005cc
YooInHak <dlsgkr2856@gmail.com>
2025-10-21 10:22:31 +0900

    2일차 시작전 커밋

--- app.js
+++ app.js
@@ -3,9 +3,28 @@ import { tasks } from './mock.js';
 
 const PORT = 3000;
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello from Express server!');
+});
+
+app.post('/tasks', (req, res) => {
+  const data = req.body;
+  console.log(data);
+  const ids = tasks.map((task) => task.id);
+  console.log(tasks);
+  const nextId = Math.max(...ids) + 1;
+  const now = new Date();
+  const newTask = {
+    ...data,
+    id: nextId,
+    createdAt: now,
+    updatedAt: now,
+    isComplete: false,
+  };
+  tasks.push(newTask);
+  res.status(201).send(newTask);
 });
 
 app.get('/tasks', (req, res) => {
@@ -17,12 +36,50 @@ app.get('/tasks', (req, res) => {
   const count = req.query.count;
   console.log('sort:', sort);
   console.log('count:', count);
-  const compareFn = sort === 'oldest' ? (a, b) => a.createdAt - b.createdAt : (a, b) => b.createdAt - a.createdAt;
-  let newTasks = tasks.sort(compareFn);
+  const compareFn =
+    sort === 'oldest'
+      ? (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+      : (a, b) => new Date(b.createdAt) - new Date(a.createdAt);
+  let newTasks = [...tasks].sort(compareFn);
   if (count) {
     newTasks = newTasks.slice(0, Number(count));
   }
   res.send(newTasks);
+});
+
+app.get('/tasks/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const task = tasks.find((task) => task.id === id);
+  if (task) {
+    res.send(task);
+  } else {
+    res.status(404).send({ message: 'Cannot find given id' });
+  }
+});
+
+app.patch('/tasks/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const task = tasks.find((task) => task.id === id);
+  if (task) {
+    Object.keys(req.body).forEach((key) => {
+      task[key] = req.body[key];
+    });
+    task.updatedAt = new Date();
+    res.send(task);
+  } else {
+    res.status(404).send({ message: 'Cannot find given id' });
+  }
+});
+
+app.delete('/tasks/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const taskIndex = tasks.findIndex((task) => task.id === id);
+  if (taskIndex !== -1) {
+    const [deletedTask] = tasks.splice(taskIndex, 1);
+    res.send(deletedTask);
+  } else {
+    res.status(404).send({ message: 'Cannot find given id' });
+  }
 });
 
 app.listen(PORT, (err) => {
--- mock.js
+++ mock.js
@@ -1,13 +1,13 @@
 export const tasks = [
   {
-    _id: '1',
+    id: 1,
     title: '30분 운동',
     isComplete: false,
     createdAt: '2023-03-23T06:34:07.617Z',
     updatedAt: '2023-03-23T06:34:07.617Z',
   },
   {
-    _id: '2',
+    id: 2,
     title: '리액트 공부',
     description: 'Tic Tac Toe 게임 완성하기',
     isComplete: true,
@@ -15,14 +15,14 @@ export const tasks = [
     updatedAt: '2023-03-23T06:34:08.617Z',
   },
   {
-    _id: '3',
+    id: 3,
     title: '집 청소',
     isComplete: true,
     createdAt: '2023-03-23T06:34:09.617Z',
     updatedAt: '2023-03-23T06:34:09.617Z',
   },
   {
-    _id: '4',
+    id: 4,
     title: '독서',
     description: '30 페이지',
     isComplete: false,
@@ -30,7 +30,7 @@ export const tasks = [
     updatedAt: '2023-03-23T06:34:10.617Z',
   },
   {
-    _id: '5',
+    id: 5,
     title: '파이썬 공부',
     description: '프로그래밍 시작하기 in Python 토픽 끝내기',
     isComplete: false,
